5c091496991a475713ee2bc052359cd6caa64fa8
Grace Sawatyanon <gs622@cornell.edu>
2025-02-26 00:40:36 -0500

    Implement player asset

--- source/SSBDudeModel.cpp
+++ source/SSBDudeModel.cpp
@@ -65,7 +65,7 @@
 /** The density of the character */
 #define DUDE_DENSITY    1.0f
 /** The impulse for the character jump */
-#define DUDE_JUMP       2.25f
+#define DUDE_JUMP       8.25f
 /** Debug color for the sensor */
 #define DEBUG_COLOR     Color4::RED
 
