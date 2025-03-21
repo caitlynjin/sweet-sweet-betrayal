16f9963f2fd6a26552a389508ecae0eefc4fcc19
jessiejia0809 <hj359@cornell.edu>
2025-03-21 15:48:25 -0400

    fixed treasure

--- source/MovePhaseScene.cpp
+++ source/MovePhaseScene.cpp
@@ -196,9 +196,10 @@ void MovePhaseScene::populate() {
     }
 
 #pragma mark : Treasure
-    _treasure = _objectController->getTreasure();
+    _treasure = std::dynamic_pointer_cast<Treasure>(
+        _networkController->createTreasureNetworked(Vec2(TREASURE_POS[0]), Size(1, 1), _scale, false)
+    );
 
-    _networkController->createTreasureNetworked(Vec2(TREASURE_POS[0]), Size(1, 1), _scale, false);
 }
 
 
