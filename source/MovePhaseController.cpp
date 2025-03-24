978ae330b27d2e787ccd6127de6f69ba51c321dd
jessiejia0809 <hj359@cornell.edu>
2025-03-23 23:18:14 -0400

    working mushroom

--- source/MovePhaseController.cpp
+++ source/MovePhaseController.cpp
@@ -226,6 +226,9 @@ void MovePhaseController::preUpdate(float dt) {
     }
 
     _movePhaseScene.preUpdate(dt);
+    if (_mushroomCooldown > 0) {
+        _mushroomCooldown--;
+    }
 }
 
 /**
@@ -471,6 +474,20 @@ void MovePhaseController::beginContact(b2Contact *contact)
         //        setFailure(true);
         _died = true;
     }
+    //ounce if we hit a mushroom
+    if ((bd1 == _movePhaseScene.getLocalPlayer().get() && bd2->getName() == "mushroom") ||
+    (bd1->getName() == "mushroom" && bd2 == _movePhaseScene.getLocalPlayer().get())) {
+
+        if (_mushroomCooldown == 0) {
+            b2Body* playerBody = _movePhaseScene.getLocalPlayer()->getBody();
+            b2Vec2 impulse(0.0f, 20.0f);
+            playerBody->ApplyLinearImpulseToCenter(impulse, true);
+
+            _mushroomCooldown = 5;
+            CULog("Mushroom bounce triggered; cooldown set to 5 frames.");
+        }
+    }
+
 
 
     // If the player collides with the growing wall, game over
