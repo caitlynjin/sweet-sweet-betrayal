1c4e7791e3fcc08ebd8e6997cbe06cc344781ed9
angelicab303 <>
2025-04-13 23:09:43 -0400

    Changed score value back

--- source/ScoreEvent.cpp
+++ source/ScoreEvent.cpp
@@ -51,7 +51,7 @@ int ScoreEvent::getScore() const {
         case END_TREASURE: // reach end with treasure: 3 points
             return 3;
         case DEAD: // dead: 0 points
-            return 10;
+            return 0;
         default:
             return 0;
     }
