591e484fe5f6eeb62ec85918c11d183cba8aaece
chvelez19 <chvelez19@gmail.com>
2025-04-30 12:21:22 -0400

    Animation Fixing

--- source/NetworkController.cpp
+++ source/NetworkController.cpp
@@ -821,6 +821,7 @@ std::pair<std::shared_ptr<physics2::Obstacle>, std::shared_ptr<scene2::SceneNode
     std::shared_ptr<Platform> movPlat = Platform::allocMoving(pos, size, pos, end, speed);
 
     auto animNode = scene2::SpriteNode::allocWithSheet(_assets->get<Texture>(GLIDING_LOG_ANIMATED), 1, 15, 15);
+    animNode->setAnchor(Vec2::ANCHOR_CENTER);
     movPlat->setPlatformAnimation(animNode, 15);
     
     movPlat->setBodyType(b2_dynamicBody);   // Must be dynamic for position to update
